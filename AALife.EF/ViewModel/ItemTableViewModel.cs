a0d20b2ff20c3b25af47962dd096c5de8ba57a1d
pyfxl <pyfxl@126.com>
2017-10-21 22:34:02 +0800

    ASP.NET 4.0 last version

---
+++ AALife.EF/ViewModel/ItemTableViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AALife.EF.ViewModel
+{
+    public class ItemTableViewModel
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int CategoryTypeID { get; set; }
+        public string CategoryTypeName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public System.DateTime ItemBuyDate { get; set; }
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public byte Recommend { get; set; }
+        public System.DateTime ModifyDate { get; set; }
+        public byte Synchronize { get; set; }
+        public Nullable<int> ItemAppID { get; set; }
+        public Nullable<int> RegionID { get; set; }
+        public string RegionType { get; set; }
+        public string RegionTypeName { get; set; }
+        public string ItemType { get; set; }
+        public string ItemTypeName { get; set; }
+        public Nullable<int> ZhuanTiID { get; set; }
+        public string ZhuanTiName { get; set; }
+        public Nullable<int> CardID { get; set; }
+        public string CardName { get; set; }
+    }
+}
